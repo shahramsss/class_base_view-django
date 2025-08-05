5ff5a9552d654d065a8cc434605aec03341dc863
shahramsss <shahramsss1987@gmail.com>
2025-08-05 12:24:34 +0330

    add options for modify headers

--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ from django.views import View
 class HomeView(View):
     def get(self, request):
         return render(request, "home/home.html")
+
+    def options(self, request, *args, **kwargs):
+        response = super().options(request, *args, **kwargs)
+        response.headers["host"] = "localhost"
+        response.headers["user"] = request.user
+        return response
